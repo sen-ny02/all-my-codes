d872caf39a63584f47f475240ec89fd5fa082fad
sen-ny02 <sen2007.maka@gmail.com>
2025-02-19 11:57:31 +0000

    Added all my relevant codes

---
+++ HTML:CSS:JS coding/REPITITION REPITITION/freeCodeCamp.js
@@ -0,0 +1,19 @@
+const character = "#";
+const count = 8;
+const rows = [];
+// console.log(rows[0])
+
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
+  }
+
+for (let i = 0; i < count; i = i+1) {
+    rows.push(padRow(i+1, count)); 
+}
+let result = ""
+
+for (const row of rows){
+    result = result + row + "\n";
+}
+
+console.log(result);
---
+++ HTML:CSS:JS coding/REPITITION REPITITION/javascript stuff.js
@@ -0,0 +1,30 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let array = [0,1,2,3,4,5];
+
+rows.push("freeCodeCamp")
+console.log(rows)
+
+let popped = rows.pop()
+console.log(popped)
+
+let pushed = rows.push("Maria")
+console.log(pushed)
+
+console.log(rows[0]) // Naomi 
+rows[2] = 10
+console.log(rows)
+let final = array[array.length - 1] // final is the last element in the array
+console.log(final)
+console.log(array)
+
+const activity = "Code! ";
+console.log(activity.repeat(3))
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+  
+const sum = addTwoNumbers(5, 10);
+console.log(sum)
