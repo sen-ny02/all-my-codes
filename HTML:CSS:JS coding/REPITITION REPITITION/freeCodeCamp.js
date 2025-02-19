badf6563a97bf4c948b7e27990299be60cc07d22
sen-ny02 <sen2007.maka@gmail.com>
2025-02-19 22:34:55 +0000

    finished code to make pyramid

--- HTML:CSS:JS coding/REPITITION REPITITION/freeCodeCamp.js
+++ HTML:CSS:JS coding/REPITITION REPITITION/freeCodeCamp.js
@@ -7,7 +7,7 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
   }
 
-for (let i=1; i < count; i++) { //i+=3 is the same as i = i+3. could also use i++; for this specific scenario. i++ is the same as i=i+1
+for (let i=1; i <= count; i++) { //i+=3 is the same as i = i+3. could also use i++; for this specific scenario. i++ is the same as i=i+1
     rows.push(padRow(i, count)); 
 }
 let result = ""
