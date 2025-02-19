3130fa75cb0d4c0c3d9826ef0e53d7f3d2f1521e
sen-ny02 <sen2007.maka@gmail.com>
2025-02-19 13:04:37 +0000

    marginal efficiency improvements

--- HTML:CSS:JS coding/REPITITION REPITITION/freeCodeCamp.js
+++ HTML:CSS:JS coding/REPITITION REPITITION/freeCodeCamp.js
@@ -7,8 +7,8 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
   }
 
-for (let i = 0; i < count; i = i+1) {
-    rows.push(padRow(i+1, count)); 
+for (let i=1; i < count; i++) { //i+=3 is the same as i = i+3. could also use i++; for this specific scenario. i++ is the same as i=i+1
+    rows.push(padRow(i, count)); 
 }
 let result = ""
 
