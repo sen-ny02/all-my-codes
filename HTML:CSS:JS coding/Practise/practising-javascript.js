d5ac9beb75ad2a68010e996b25d580f607d67c95
sen-ny02 <sen2007.maka@gmail.com>
2025-02-20 19:34:29 +0000

    did some more js practise

--- HTML:CSS:JS coding/Practise/practising-javascript.js
+++ HTML:CSS:JS coding/Practise/practising-javascript.js
@@ -1,5 +1,6 @@
 const emptyChar = ""
-
+const myArray1 = [1, 4, 13, 75, 8, 91]
+const myArray2 = [1,2,3,4,5,6,7,8,9,10]
 
 console.log("Numbers 1 - 10 using for loop")
 for (let i = 1; i<= 10; i+=1) {
@@ -30,3 +31,36 @@ for(let i = 11; i <= 30; i += 2)
     sum += i;
 }
 console.log(sum);
+
+console.log("a function that will convert from Fahrenheit to Celsius")
+function fahrenheitToCel(fahrenheit){
+    celcius = (fahrenheit - 30)/2
+    return celcius
+}
+let testConverter = fahrenheitToCel(57)
+console.log(testConverter)
+
+console.log("Calculate the sum of numbers in an array of numbers")
+
+let Sum = 0
+function findArrSum(array){
+    for(let i=0; i<array.length; i++){
+        Sum = Sum + array[i]
+    }
+    return Sum
+}
+
+callFindArrSum = findArrSum(myArray1)
+console.log(callFindArrSum)
+
+console.log("Calculate the average of the numbers in an array of numbers")
+
+let total = 0
+function findAverage(array){
+    for (let i=0; i<array.length; i++){
+        total = total+array[i]
+    }
+    return average = total/ArrayBuffer.length
+}
+
+console.log(findAverage(myArray2))
