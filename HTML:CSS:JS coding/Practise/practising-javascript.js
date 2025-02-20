8221433fb9bef6004092a7d57358fdc495719632
sen-ny02 <sen2007.maka@gmail.com>
2025-02-20 12:47:17 +0000

    added new challenge

--- HTML:CSS:JS coding/Practise/practising-javascript.js
+++ HTML:CSS:JS coding/Practise/practising-javascript.js
@@ -23,3 +23,10 @@ for (let num1 = 1; num1<=10; num1++){
     }
 }
 
+console.log("Calculate the sum of odd numbers greater than 10 and less than 30")
+let sum = 0;
+for(let i = 11; i <= 30; i += 2)
+{
+    sum += i;
+}
+console.log(sum);
