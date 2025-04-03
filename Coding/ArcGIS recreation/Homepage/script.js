2a8183530ec7841a4e8314c27d8a2cf979b1b0da
sen-ny02 <sen2007.maka@gmail.com>
2025-04-03 16:56:30 +0100

    made the button work using js

---
+++ Coding/ArcGIS recreation/Homepage/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('sign-in-button');
+button.addEventListener("click", goToSignIn)
+
+function goToSignIn(){
+    window.location.href = "/Users/senitha/Desktop/GitHub/GitHub/all-my-codes/Coding/ArcGIS recreation/Sign in/index.html"
+}
